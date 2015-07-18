7364141e793a0c8366fa5ab574f31803689b8f9b
bishnud <bishnudasindia@gmail.com>
2015-07-18 21:06:18 +0530

    Just created project

--- Storyboard/Storyboard/Properties/AssemblyInfo.cs
+++ Storyboard/Storyboard/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("244afd26-cd1b-450f-8a46-4e2b0dd4cf59")]
+[assembly: Guid("0f03fd44-0d92-4676-995e-ab3e00edd450")]
 
 // Version information for an assembly consists of the following four values:
 //
