fd9a848151b6ce2e2e143f11ecc99b3525bdbe6b
bishnud <bishnudasindia@gmail.com>
2015-08-01 20:33:59 +0530

    added directives, http

---
+++ Miscellaneous/AngularJS/Scripts/controllers.js
@@ -0,0 +1,36 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+})
+
+var personAppl = angular.module('personApp', []);
+personAppl.controller('personCtrl', function ($scope) {
+    $scope.firstName = "John C";
+    $scope.lastName = "Doe";
+
+    $scope.fullName = function () {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+})
+
+var personAppObjects = angular.module('personAppObjects', []);
+personAppObjects.controller('personCtrlObjects', function ($scope) {
+    $scope.persons = [
+        { name: 'Marcia', phone: 123456 },
+        { name: 'Bishnu', phone: 10543234 },
+        { name: 'Amol', phone: 2424234 }
+    ];
+});
+
+var customerAppObject = angular.module('customersApp', []);
+customerAppObject.controller('customersController', function ($scope, $http) {
+    var requestUrl = 'http://www.w3schools.com/angular/customers.php';
+    $http.get(requestUrl)
+    .success(function (response) {
+        $scope.customers = response.records;
+    });
+});
+
+
+
